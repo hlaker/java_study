37163a32471a1b09de84706e069edaf17bd2aeb5
hlaker <gaeman0411@gmail.com>
2023-02-06 19:44:52 +0900

    good

---
+++ src/main/java/net/kim/java_study/go.java
@@ -0,0 +1,16 @@
+package net.kim.java_study;
+
+public class go {
+
+    public static void main(String[] arg){
+        int num;
+        num=20;
+        System.out.print(num);
+        System.out.println("hello world");
+
+    }
+
+
+
+
+}
